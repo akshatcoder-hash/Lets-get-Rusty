484afba60590597c40bc733607d693eeb5734be7
Akshat Sharma <akshatsharma@pop-os.localdomain>
2023-01-22 19:59:11 +0530

    Hello Cargo 🦀

---
+++ hello_world/hello_cargo/target/debug/deps/hello_cargo-86b856d3e5cfd6ec.d
@@ -0,0 +1,5 @@
+/home/akshatsharma/Lets-get-Rusty/hello_world/hello_cargo/target/debug/deps/hello_cargo-86b856d3e5cfd6ec: src/main.rs
+
+/home/akshatsharma/Lets-get-Rusty/hello_world/hello_cargo/target/debug/deps/hello_cargo-86b856d3e5cfd6ec.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/akshatsharma/Lets-get-Rusty/hello_world/hello_cargo/target/debug/hello_cargo: /home/akshatsharma/Lets-get-Rusty/hello_world/hello_cargo/src/main.rs
