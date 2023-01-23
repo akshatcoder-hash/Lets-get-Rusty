fe69a25eca2333c529080e261910011d62f1cab0
Akshat Sharma <akshatcoder@outlook.com>
2023-01-23 13:59:27 +0000

    Basic Data Types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,4 +5,18 @@ fn main() {
     println!("{}", x);
 
     const SUBSCRIBER_COUNT: u32 = 100_000;
+
+    // Data Types:
+
+    // Integers
+    let a = 98_222; // Decimal
+    let b = b'A'; // Byte
+    
+    // Floating Point
+    let f = 2.0;
+
+    let t = true;
+
+
+    // Compound Types
 }
