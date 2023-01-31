bc0b558c1c3c67561361c64a8e14c406e499a639
Akshat Sharma <akshatsharma@pop-os.localdomain>
2023-02-01 01:46:32 +0530

    Functions for  ownership 🦀

--- Ownership/src/main.rs
+++ Ownership/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    fn a()  {
+        let x = "hello";
+        let y = 22;
+        b();
+    }
+
+    fn b(){
+        let x = String::from("world");
+    }
 }
