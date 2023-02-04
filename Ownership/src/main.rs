0e2bbfca578b786532fa8ec576e7a01a81d8b5cd
Akshat Sharma <akshatcoder@outlook.com>
2023-02-04 13:20:20 +0000

    🦀 Created Ownership

--- Ownership/src/main.rs
+++ Ownership/src/main.rs
@@ -1,11 +1,3 @@
 fn main() {
-    fn a()  {
-        let x = "hello";
-        let y = 22;
-        b();
-    }
-
-    fn b(){
-        let x = String::from("world");
-    }
+    println!("Hello, world!");
 }
