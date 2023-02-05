d8f28b1ccca6ec0168ad092b7f6851f13e31ed90
Akshat Sharma <akshatcoder@outlook.com>
2023-02-05 20:16:39 +0530

    :art: Reformated the code

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,8 +1,7 @@
 fn main() {
+    // s is not valid here, it’s not yet declared
+    let s = "hello"; // s is valid from this point forward
 
-        // s is not valid here, it’s not yet declared
-        let s = "hello"; // s is valid from this point forward
-
-        // do stuff with s
-        // this scope is now over, and s is no longer valid
+    // do stuff with s
+    // this scope is now over, and s is no longer valid
 }
